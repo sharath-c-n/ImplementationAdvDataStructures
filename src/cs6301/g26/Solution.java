a8275fa83ed8e2063f9fb1bfa8b38ff208884a17
Sandeep Avula <avulasandeep999@gmail.com>
2017-08-30 01:16:59 -0500

    Updated Code  - sxa165930

--- src/cs6301/g26/Solution.java
+++ src/cs6301/g26/Solution.java
@@ -5,73 +5,105 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Queue;
 
+//Class Vertex-Record Stores the Vertex and depth relative to the Start node of BFS
+
+class Vertex_Record {
+    private Graph.Vertex ver;
+    private int height;
+
+    Vertex_Record(Graph.Vertex v, int dep) {
+        this.ver = v;
+        this.height = dep;
+    }
+
+    public Graph.Vertex Get_Vertex() {
+        return this.ver;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+}
+
 public class Solution {
-    public static Scanner in;
-    public static boolean[] visit;
-    public static int vertices;
-    public static Graph.Vertex max_node_root;
+    private static boolean[] visit;
+    private static int[] dist;
 
     Solution(int size) {
-        vertices = size;
-        visit = new boolean[vertices + 1];
+        dist = new int[size + 1];
+        visit = new boolean[size + 1];
         Arrays.fill(visit, Boolean.FALSE);
     }
 
-    public static void BFS(Graph ss, Graph.Vertex node) {
-        Queue<Graph.Vertex> q = new LinkedList<Graph.Vertex>();
-        q.add(node);
+    //BFS  with parameters start node and condition is set to true to store dist of a node from the start node.
+    private static Graph.Vertex BFS(Graph.Vertex node, boolean cond) {
+        Graph.Vertex max_node_root = null;
+        Queue<Vertex_Record> q = new LinkedList<>();
+        q.add(new Vertex_Record(node, 0));
+        if (cond)
+            dist[node.name] = 0;
         while (!q.isEmpty()) {
-            Graph.Vertex top_record = q.poll();
-            visit[top_record.name] = true;
-            max_node_root = top_record;
-            for (Graph.Edge mss : top_record.adj) {
-                if (visit[mss.from.getName()] == false) {
-                    q.add(mss.from);
-                } else if (visit[mss.to.getName()] == false) {
-                    q.add(mss.to);
+            Vertex_Record top_record = q.poll();
+            visit[top_record.Get_Vertex().name] = true;
+            max_node_root = top_record.Get_Vertex();
+            for (Graph.Edge mss : top_record.Get_Vertex().adj) {
+                if (!visit[mss.from.getName()]) {
+                    q.add(new Vertex_Record(mss.from, top_record.getHeight() + 1));
+                    if (cond)
+                        dist[mss.from.name] = top_record.getHeight() + 1;
+
+                } else if (!visit[mss.to.getName()]) {
+                    q.add(new Vertex_Record(mss.to, top_record.getHeight() + 1));
+                    if (cond)
+                        dist[mss.to.name] = top_record.getHeight() + 1;
                 }
             }
         }
+        return max_node_root;
     }
 
-    public static LinkedList<Graph.Vertex> diameter(Graph g) {
+    private static LinkedList<Graph.Vertex> diameter(Graph g) {
 
         LinkedList<Graph.Vertex> res = new LinkedList<>();
         if (g.n < 1)
             return res;
-        BFS(g, g.getVertex(1));
-        Graph.Vertex start_node = max_node_root;
+        //Choose vertex 1 as arbitary vertex
+
+        Graph.Vertex start_node = BFS(g.getVertex(1), false);
         Arrays.fill(visit, Boolean.FALSE);
-        BFS(g, max_node_root);
-        Graph.Vertex prev_node = max_node_root;
+        Graph.Vertex prev_node = BFS(start_node, true);
         res.add(prev_node);
         while (prev_node.name != start_node.name) {
-            for (Graph.Edge e : prev_node.adj) {
-                if (e.from.name != prev_node.name) {
-                    prev_node = e.from;
-                    res.add(e.from);
-                    break;
-                }
-                if (e.to.name != prev_node.name) {
-                    prev_node = e.to;
-                    res.add(e.to);
-                    break;
+            int mx_ans = dist[prev_node.name];
+            Graph.Vertex temp = prev_node;
+            for (Graph.Edge e : temp.adj) {
+                if (e.from.name != temp.name) {
+                    if (dist[e.from.name] == mx_ans - 1) {
+                        prev_node = e.from;
+                        break;
+                    }
+                } else if (e.to.name != temp.name) {
+                    if (dist[e.to.name] == mx_ans - 1) {
+                        prev_node = e.to;
+                        break;
+                    }
                 }
             }
+            res.add(prev_node);
         }
         return res;
     }
 
     public static void main(String[] args) {
-        in= new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         Graph inp = Graph.readGraph(in);
         Solution inst = new Solution(inp.n);
         LinkedList<Graph.Vertex> ans = diameter(inp);
-        System.out.println(" the longest diameter is ");
+        System.out.println(" The longest path is ");
         for (Graph.Vertex v : ans) {
             System.out.print(v);
-            if(v!=ans.getLast())
-            System.out.print("  ->  ");
+            if (v.name != ans.getLast().name)
+                System.out.print("  ->  ");
         }
         System.out.println("\n");
     }
