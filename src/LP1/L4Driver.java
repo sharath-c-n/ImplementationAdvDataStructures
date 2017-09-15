4df05a794ed77f112a73406d8b2cc8928fae6f73
sharath-c-n <sharathcn@outlook.com>
2017-09-14 22:48:51 -0500

    Fixing Level 4

--- src/LP1/L4Driver.java
+++ src/LP1/L4Driver.java
@@ -50,7 +50,6 @@ public class L4Driver {
             } else if (state == 2 && Token.EQ == token) {
                 state++;
             } else if (state == 2 && Token.EOL == token) {
-                System.out.println(variableMap.get(variable));
                 expression.add(variable);
                 addLabelEntry(variable, expression, label, isLabel);
                 expression.clear();
@@ -60,16 +59,14 @@ public class L4Driver {
                 expression.add(variable);
                 expression.add(symbol);
             } else if (state == 3 && Token.NUM == token) {
-                variableMap.put(variable, new Num(symbol));
-                if (isLabel)
-                    expression.add(symbol);
+                variableMap.put("t" + constantsCount, new Num(symbol));
+                expression.add("t" + constantsCount++);
                 state++;
             } else if (state == 3 && Token.VAR == token) {
                 state = 5;
                 expression.add(symbol);
             } else if (state == 4 && Token.EOL == token) {
                 addLabelEntry(variable, expression, label, isLabel);
-                System.out.println(variableMap.get(variable));
                 expression.clear();
                 isLabel = false;
                 state = 0;
@@ -83,9 +80,6 @@ public class L4Driver {
                     expression = ShuntingYard.parse(expression);
                 }
                 addLabelEntry(variable, expression, label, isLabel);
-                variableMap.put(variable, PostfixEvaluatorV2.evaluate(variableMap, expression));
-                if(!isLabel)
-                    System.out.println(variableMap.get(variable));
                 expression.clear();
                 isLabel = false;
                 state = 0;
@@ -97,17 +91,42 @@ public class L4Driver {
                 expression.add(symbol);
             } else if ((state == 7 || state == 9) && Token.EOL == token) {
                 addLabelEntry(null, expression, label, isLabel);
-                executeGoto(expression);
                 isLabel = false;
                 expression.clear();
                 state = 0;
             } else if (state == 0 && Token.EOL == token) {
-                variableMap.get(variable).printList();
                 break;
             } else {
                 throw new IllegalStateException("Fatal error cannot continue!!");
             }
         }
+        execute();
+    }
+
+    private void execute() {
+        int pc = 0;
+        LabelEntry entry=null;
+        while(pc!=labelEntries.size()){
+            entry = labelEntries.get(pc++);
+            if (entry.variable != null) {
+                variableMap.put(entry.variable, PostfixEvaluatorL4.evaluate(variableMap, entry.expression).clone());
+                if(!entry.isLabel){
+                    System.out.println(variableMap.get(entry.variable));
+                }
+            } else {
+                executeGoto(entry.expression);
+            }
+        }
+        if(entry !=null){
+            if( entry.variable != null){
+                variableMap.get(entry.variable).printList();
+            }
+            else
+            {
+                variableMap.get(entry.expression.peek()).printList();
+            }
+        }
+
     }
 
     private Token tokenize(String s) throws Exception {
@@ -147,7 +166,7 @@ public class L4Driver {
         while (branchTo != -1 && labelEntries.size() > branchTo) {
             LabelEntry entry = labelEntries.get(branchTo);
             if (entry.variable != null) {
-                variableMap.put(entry.variable, PostfixEvaluatorV2.evaluate(variableMap, entry.expression));
+                variableMap.put(entry.variable, PostfixEvaluatorL4.evaluate(variableMap, entry.expression));
                 if(!entry.isLabel){
                     System.out.println(variableMap.get(entry.variable));
                 }
@@ -192,7 +211,7 @@ public class L4Driver {
      */
 
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(new File("./src/LP1/lp1-l4-in2.txt"));
+        Scanner in = new Scanner(new File("./src/LP1/lp1-l4-in3.txt"));
         L4Driver l4Driver = new L4Driver();
         l4Driver.runStateMachine(in);
     }
---
+++ src/LP1/PostfixEvaluatorL4.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * This class evaluates the expression expression and sends the result back to the caller
+ */
+public class PostfixEvaluatorL4 extends PostfixEvaluator{
+    /**
+     * This function is called to evaluate a expression expression
+     * @param valueMap   : a table to store the values of a variable
+     * @param postfixExp : expressed to be evaluated
+     * @return : value of the expression
+     * @exception IllegalArgumentException : thrown when the value table doesnt have the symbol that is referred in the
+     * expression.
+     */
+    public static Num evaluate(Map<String, Num> valueMap, Queue<String> postfixExp) {
+        Stack<Num> stack = new Stack<>();
+        for (String cur:postfixExp) {
+            if (!Operators.isOperator(cur.charAt(0))) {
+                Num value = valueMap.get(cur);
+                if(value == null){
+                    throw new IllegalArgumentException("Argument not defined in expression : " + postfixExp);
+                }
+                stack.push(value);
+            } else {
+                Num right = stack.pop();
+                Operators operators = Operators.getByValue(cur.charAt(0));
+                Num left = null;
+                if (operators.isBinary()) {
+                    left = stack.pop();
+                }
+                stack.push(operate(left, right, operators));
+            }
+        }
+        return stack.pop();
+    }
+}
