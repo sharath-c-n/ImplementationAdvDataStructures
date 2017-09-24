b7ca1ba6aa148bd07b426969a9563c9c862a2d5e
sharath-c-n <sharathcn@outlook.com>
2017-09-24 14:20:19 -0500

    Adding merge sort

--- src/SP4/cs6301/g26/KMissing.java
+++ src/SP4/cs6301/g26/KMissing.java
@@ -1,5 +1,8 @@
 package cs6301.g26;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Finding the K missing elements in a sorted array.
  * @author Sharath
@@ -9,8 +12,8 @@ public class KMissing {
      * Public function to find the K missing elements
      * @param a : sorted array which has elements missing
      */
-    public static void findKMissing(int a[]) {
-        findKMissing(a, 0, a.length - 1);
+    public static void findKMissing(int a[], List<Integer> missing) {
+        findKMissing(a, 0, a.length - 1,missing);
     }
 
     /**
@@ -19,23 +22,23 @@ public class KMissing {
      * @param low : Starting index of the sub-array
      * @param high : last index of the sub-array
      */
-    static void findKMissing(int a[], int low, int high) {
+    static void findKMissing(int a[], int low, int high,List<Integer> missing) {
         if (high >= low) {
             //finally check if any elements are missing between 2 elements
             if (high - low == 1) {
                 for (int i = a[low] + 1; i < a[high]; i++) {
-                    System.out.print(i + " ");
+                    missing.add(i);
                 }
                 return;
             }
             int mid = low + (high - low) / 2;
             //Check if any elements are missing in the right half
             if ((a[mid] + high - mid) != a[high]) {
-                findKMissing(a, mid, high);
+                findKMissing(a, mid, high,missing);
             }
             //Check if any elements are missing in the left half
             if ((a[low] + mid - low) != a[mid]) {
-                findKMissing(a, low, mid);
+                findKMissing(a, low, mid,missing);
             }
         }
     }
@@ -45,8 +48,10 @@ public class KMissing {
      * @param args : command line arguments
      */
     public static void main(String[] args) {
-        int[] a = {1, 2, 4, 5, 6, 10};
-        findKMissing(a);
+        int[] a = {1, 2, 4, 8, 12};
+        ArrayList<Integer> missingElements = new ArrayList<>();
+        findKMissing(a,missingElements);
+        missingElements.forEach(x->System.out.print(x+ " "));
     }
 
 }
