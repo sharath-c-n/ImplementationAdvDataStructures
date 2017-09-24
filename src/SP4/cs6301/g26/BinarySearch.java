5c16e9977fd461ab98ef70d70d4591d45dd1aa10
sharath-c-n <sharathcn@outlook.com>
2017-09-23 19:08:42 -0500

    Adding more comments

--- src/SP4/cs6301/g26/BinarySearch.java
+++ src/SP4/cs6301/g26/BinarySearch.java
@@ -1,10 +1,30 @@
 package cs6301.g26;
 
+/**
+ * This class will do a recursive binary search for any generic elements in a sorted array
+ * @author Ankitha
+ */
 public class BinarySearch {
+    /**
+     * This is the public function to perform a binary search on a sorted generic array.
+     * @param arr : Sorted generic array
+     * @param x : Generic element to be searched in the array
+     * @param <T> : the generic element should implement comparable
+     * @return : the element index in the provided array
+     */
     public static<T extends Comparable<? super T>> int binarySearch(T[] arr, T x){
         return customBinarySearch(arr,0,arr.length,x);
     }
 
+    /**
+     * This is the public function to perform a binary search on a sorted generic array.
+     * @param arr : Sorted generic array
+     * @param low : Starting index of the generic sub-array
+     * @param high : Ending index of the generic sub-array
+     * @param x : Element to find
+     * @param <T> : Generic type
+     * @return : The element index in the provided array if found else -1.
+     */
     public static<T extends Comparable<? super T>> int customBinarySearch(T[] arr,int low,int high ,T x) {
         {
             if (high >= low) {
@@ -13,11 +33,10 @@ public class BinarySearch {
                 // If the element is present at the middle itself
                 if (x.compareTo(arr[mid]) >= 0 && x.compareTo(arr[mid + 1]) < 0) return mid;
 
-                // If element is smaller than mid, then it can only be present
-                // in left subarray
+                // If element is smaller than mid
                 if (x.compareTo(arr[mid]) < 0) return customBinarySearch(arr, low, mid - 1, x);
 
-                // Else the element can only be present in right subarray
+                //At his point element can only be present in right sub-array
                 return customBinarySearch(arr, mid + 1, high, x);
             }
 
--- src/SP4/cs6301/g26/Fibonacci.java
+++ src/SP4/cs6301/g26/Fibonacci.java
@@ -3,37 +3,53 @@ package cs6301.g26;
 import java.math.BigInteger;
 
 public class Fibonacci {
-    // Do a simple linear scan algorithm: Fib[n] = Fin[n-1] + Fin[n-2];
-    // Since numbers are stored with BigInteger class, use add for "+"
+    /**
+     * A simple linear scan algorithm: Fib[n] = Fin[n-1] + Fin[n-2]; to find the asked
+     * fibonacci number
+     *
+     * @param n : The fibonacci number position in the series
+     * @return : The fibonacci number
+     */
     static BigInteger linearFibonacci(int n) {
         BigInteger prev = new BigInteger("1");
         BigInteger prev2 = new BigInteger("0");
-        BigInteger temp ;
-        for(int i =2;i<n;i++){
+        BigInteger temp;
+        for (int i = 2; i < n; i++) {
             temp = prev.add(prev2);
             prev2 = prev;
             prev = temp;
         }
-       return prev;
+        return prev;
 
     }
 
-    public static  BigInteger[][] createMatrix(int row,int col){
+    /**
+     * Create a zero matrix of specified size
+     * @param row : Required number of rows
+     * @param col : Required number of column
+     * @return : Zero matrix of size row x col
+     */
+    public static BigInteger[][] createMatrix(int row, int col) {
         BigInteger[][] matrix = new BigInteger[row][col];
-        for(int i=0;i<row;i++)
-            for (int j=0;j<col;j++)
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
                 matrix[i][j] = new BigInteger("0");
         return matrix;
     }
 
-    // return c = a * b
+    /**
+     * Performs multiplication of 2 matrices
+     * @param a : matrix a
+     * @param b : matrix b
+     * @return : product of A and b
+     */
     public static BigInteger[][] multiply(BigInteger[][] a, BigInteger[][] b) {
         int m1 = a.length;
         int n1 = a[0].length;
         int m2 = b.length;
         int n2 = b[0].length;
         if (n1 != m2) throw new RuntimeException("Illegal matrix dimensions.");
-        BigInteger[][] c = createMatrix(m1,n2);
+        BigInteger[][] c = createMatrix(m1, n2);
         for (int i = 0; i < m1; i++)
             for (int j = 0; j < n2; j++)
                 for (int k = 0; k < n1; k++)
@@ -41,40 +57,50 @@ public class Fibonacci {
         return c;
     }
 
-    static BigInteger[][]  power(BigInteger[][] x, int y)
-    {
+    /**
+     * Finds the power of a matrix by using divide and conquer algorithm
+     * @param x : the matrix whose power have to me found
+     * @param y : power
+     * @return : a matrix which is multiplied with itself y times.
+     */
+    static BigInteger[][] power(BigInteger[][] x, int y) {
         if (y == 0) {
             return getIdentityMat(x[0].length);
-        } else if (y%2 == 0)
-            return multiply(power(x, y/2),power(x, y/2));
+        } else if (y % 2 == 0)
+            return multiply(power(x, y / 2), power(x, y / 2));
         else
-            return multiply(x,multiply(power(x, y/2),power(x, y/2)));
+            return multiply(x, multiply(power(x, y / 2), power(x, y / 2)));
     }
 
+    /**
+     * Return an Identity matrix of the required size.
+     * @param n : the size of the square matrix required.
+     * @return : Identity matrix of size n x n.
+     */
     static private BigInteger[][] getIdentityMat(int n) {
         BigInteger[][] identityMatrix = new BigInteger[n][n];
-        for(int i =0;i<n;i++){
-            for(int j= 0 ;j<n;j++){
-                if(i==j){
-                    identityMatrix[i][j] = BigInteger.valueOf(1);
-                }
-                else {
-                    identityMatrix[i][j] = BigInteger.valueOf(0);
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                identityMatrix[i][j] = i == j ? new BigInteger("1") : new BigInteger("0");
             }
         }
         return identityMatrix;
     }
 
-    // Implement O(log n) algorithm described in class (Sep 15)
+    /**
+     * A simple log(n) algorithm to find the asked
+     * fibonacci number
+     * @param n : The fibonacci number position in the series
+     * @return : The fibonacci number
+     */
     static BigInteger logFibonacci(int n) {
-        BigInteger[][] fibMatrix = {{ BigInteger.valueOf(1), BigInteger.valueOf(1)},{ BigInteger.valueOf(1), BigInteger.valueOf(0)}};
-        BigInteger[][] multiplier = {{ BigInteger.valueOf(1)},{ BigInteger.valueOf(0)}};
-        BigInteger[][] result = multiply(power(fibMatrix,n-2),multiplier);
+        BigInteger[][] fibMatrix = {{BigInteger.valueOf(1), BigInteger.valueOf(1)}, {BigInteger.valueOf(1), BigInteger.valueOf(0)}};
+        BigInteger[][] multiplier = {{BigInteger.valueOf(1)}, {BigInteger.valueOf(0)}};
+        BigInteger[][] result = multiply(power(fibMatrix, n - 2), multiplier);
         return result[0][0];
     }
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         int fibNum = 1000000;
         System.out.println(logFibonacci(fibNum));
         System.out.println(linearFibonacci(fibNum));
