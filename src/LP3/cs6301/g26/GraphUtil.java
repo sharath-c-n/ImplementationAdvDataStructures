db10e4d9dd97a48aeff62cbc6968314d3aed4421
Sanasdfasd <avulasandeep999@gmail.com>
2017-10-22 20:08:24 -0500

    test

---
+++ src/LP3/cs6301/g26/GraphUtil.java
@@ -0,0 +1,119 @@
+package cs6301.g26;
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author  Sharath
+ * This class contains graph utility functions
+ */
+public class GraphUtil extends GraphAlgorithm<GraphUtil.Vertex> {
+
+    public GraphUtil(Graph g) {
+        super(g);
+        node = new Vertex[g.size()];
+        for (Graph.Vertex u : g) {
+            setVertex(u,new Vertex());
+        }
+    }
+
+    // reinitialize allows running multiple algorithms many times.
+    void reinitialize() {
+        for (Graph.Vertex u : g) {
+            Vertex utilVertex = getVertex(u);
+            utilVertex.isVisited = false;
+        }
+    }
+
+    public class Vertex {
+        boolean isVisited;
+        public Vertex() {
+            this.isVisited = false;
+        }
+    }
+
+
+    /**
+     * This function will run the topological sort on all vertices of the input graph.
+     * been explored so far
+     *
+     * @return : The number of Strongly connected components in the graph
+     */
+
+    public boolean isStronglyConnected() {
+        int count = 0;
+        reinitialize();
+        //Step 1 : find the topological oder of the vertices of the given graph
+        List<Graph.Vertex> path = topologicalSort();
+        reinitialize();
+        //Step 2 : do a reverseDFS on the reverse graph by picking each vertex from the path in the topological order
+        //Step 3: Increment the count of Strongly Connected Components when a vertex which is
+        // not explored previously is found indicating the discovery of a new component
+        for (Graph.Vertex v : path) {
+            if (!getVertex(v).isVisited) {
+                count++;
+                reverseDFS(v);
+            }
+        }
+        return count == 1;
+    }
+
+    private void reverseDFS(Graph.Vertex v) {
+        getVertex(v).isVisited = true;
+        //reverse graph is got by traversing over the reverse adjacency list which is maintained
+        for (Graph.Edge e : v.revAdj) {
+            if (!getVertex(e.otherEnd(v)).isVisited) {
+                reverseDFS(e.otherEnd(v));
+            }
+        }
+    }
+
+    /**
+     * Finds the topological ordering of the input graph
+     * @return topological order
+     */
+    public List<Graph.Vertex> topologicalSort() {
+        //output is stored the order of highest discovery time
+        reinitialize();
+        ArrayDeque<Graph.Vertex> list = new ArrayDeque<Graph.Vertex>();
+        for (Graph.Vertex vertex : g) {
+            if (!getVertex(vertex).isVisited) {
+                topologicalSortUtil(vertex, list);
+            }
+        }
+        return new ArrayList<Graph.Vertex>(list);
+    }
+
+    /**
+     * This function will run the topological sort on all vertices of the input graph.
+     * @param vertex : graph vertex
+     * @param list   : output is stored the order of highest discovery time
+     */
+    private void topologicalSortUtil(Graph.Vertex vertex, ArrayDeque<Graph.Vertex> list) {
+        getVertex(vertex).isVisited = true;
+        for (Graph.Edge e : vertex) {
+            if (!getVertex(e.otherEnd(vertex)).isVisited) {
+                topologicalSortUtil(e.otherEnd(vertex), list);
+            }
+        }
+        list.addFirst(vertex);
+    }
+
+    /**
+     * Checks if indegree and outdegree of all the vertices are same
+     * @return null if all hte vertices have same indegree and out degree count or returns the
+     * first vertex with different indegree and outdegree count
+     */
+    Graph.Vertex inEqlOutEdges() {
+        for (Graph.Vertex vertex : g) {
+            if (vertex.revAdj.size() != vertex.adj.size()) {
+                return vertex;
+            }
+        }
+        return null;
+    }
+
+}
--- src/LP3/cs6301/g26/LP3.java
+++ src/LP3/cs6301/g26/LP3.java
@@ -7,13 +7,15 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
 
-import cs6301.g00.Graph;
-import cs6301.g00.Graph.Vertex;
-import cs6301.g00.Graph.Edge;
-import cs6301.g00.Timer;
+import cs6301.g26.Graph;
+import cs6301.g26.Graph.Vertex;
+import cs6301.g26.Graph.Edge;
+import cs6301.g26.Timer;
+import cs6301.g26.XGraph;
 
 public class LP3 {
-    static int VERBOSE = 1;
+    static int VERBOSE = 0;
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in;
         if (args.length > 0) {
@@ -22,7 +24,7 @@ public class LP3 {
         } else {
             in = new Scanner(System.in);
         }
-        if(args.length > 1) {
+        if (args.length > 1) {
             VERBOSE = Integer.parseInt(args[1]);
         }
 
@@ -36,9 +38,9 @@ public class LP3 {
         timer.end();
 
         System.out.println(wmst);
-        if(VERBOSE > 0) {
+        if (VERBOSE > 0) {
             System.out.println("_________________________");
-            for(Edge e: dmst) {
+            for (Edge e : dmst) {
                 System.out.print(e);
             }
             System.out.println();
@@ -47,15 +49,32 @@ public class LP3 {
         System.out.println(timer);
     }
 
-    /** TO DO: List dmst is an empty list. When your algorithm finishes,
-     *  it should have the edges of the directed MST of g rooted at the
-     *  start vertex.  Edges must be ordered based on the vertex into
-     *  which it goes, e.g., {(7,1),(7,2),null,(2,4),(3,5),(5,6),(3,7)}.
-     *  In this example, 3 is the start vertex and has no incoming edges.
-     *  So, the list has a null corresponding to Vertex 3.
-     *  The function should return the total weight of the MST it found.
+    /**
+     * TO DO: List dmst is an empty list. When your algorithm finishes,
+     * it should have the edges of the directed MST of g rooted at the
+     * start vertex.  Edges must be ordered based on the vertex into
+     * which it goes, e.g., {(7,1),(7,2),null,(2,4),(3,5),(5,6),(3,7)}.
+     * In this example, 3 is the start vertex and has no incoming edges.
+     * So, the list has a null corresponding to Vertex 3.
+     * The function should return the total weight of the MST it found.
      */
     public static int directedMST(Graph g, Vertex start, List<Edge> dmst) {
+
+        XGraph xg = new XGraph(g);
+        Vertex src = xg.getVertex(1);
+        BFSZeroEdge zeroGraph = new BFSZeroEdge(xg, src);
+        zeroGraph.computeZeroEdgeGraph();
+        printGraph(g);
+        SCC ns= new SCC(xg,src);
+        ns.Connected();
         return 0;
+    }
+
+    static void printGraph(Graph g) {
+        for (Graph.Vertex v : g) {
+            for (Graph.Edge e : v) {
+                System.out.println(v + " " + e.otherEnd(v) + " " + e.weight);
+            }
+        }
     }
 }
